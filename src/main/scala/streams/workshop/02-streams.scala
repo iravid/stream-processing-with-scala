f1b9aeb328fa1f69122e0bc780beb90a91475b65
Itamar Ravid <iravid@iravid.com>
2020-06-23 19:02:30 +0300

    Second day solutions

--- src/main/scala/streams/workshop/02-streams.scala
+++ src/main/scala/streams/workshop/02-streams.scala
@@ -1,20 +1,32 @@
 package streams.workshop
 
 import zio._
+import zio.duration._
 import zio.clock.Clock
 import zio.stream._
 import java.nio.file.Path
 import java.io.IOException
 
 object StreamTypes {
+
+  // val st: ZStream[console.Console, Nothing, Int] = ???
+  // val st2: ZStream[Any, Nothing, Nothing] = ???
+  // val z: ZIO[Any, Nothing, Nothing] = ???
+
   // 1. A stream that emits integers and cannot fail.
+  type IntStream = ZStream[Any, Nothing, Int]
 
   // 2. A stream that emits strings and can fail with throwables.
+  type StringThrowableStream = ZStream[Any, Throwable, String]
 
-  // 3. A stream that emits no elements.
+  // 3. A stream that emits no elements and could fail with integers.
+  type NoElements = ZStream[Any, Int, Nothing]
 
   // 4. A stream that requires access to the console, can fail with
   // string errors and emits integers.
+  type Final = ZStream[console.Console, String, Int]
+
+  // type Stream[A] = ZIO[Any, Nothing, (A, Stream[A])]
 }
 
 object ConstructingStreams {
@@ -23,52 +35,95 @@ object ConstructingStreams {
   // for streams. In this section, we will survey various ways to construct
   // streams.
 
+  val st = ZStream(1, 2, 3)
+
+  val s: ZStream[random.Random, Nothing, Int] = ZStream.fromEffect(random.nextInt)
+
   // 1. Construct a stream with a single integer, '42'.
-  val single: ??? = ???
+  val single: ZStream[Any, Nothing, Int] = ZStream(42)
 
   // 2. Construct a stream with three characters, 'a', 'b', 'c'.
-  val chars: ??? = ???
+  val chars: ZStream[Any, Nothing, Char] = ZStream('a', 'b', 'c')
 
   // 3. Construct a stream from an effect that reads a line from the user.
-  val readLine: ??? = ???
+  val readLine: ZStream[console.Console, IOException, String] = ZStream.fromEffect(console.getStrLn)
 
   // 4. Construct a stream that fails with the string "boom".
-  val failed: ??? = ???
+  val failed: ZStream[Any, String, Nothing] = ZStream.fail("Boom")
 
   // 5. Create a stream that extracts the Clock from the environment.
-  val clockStream: ZStream[???, ???, Clock] = ???
+  val zio: ZIO[Clock, Nothing, Clock]             = ZIO.environment[Clock]
+  val clockStream: ZStream[Clock, Nothing, Clock] = ZStream.environment[Clock]
 
   // 6. Construct a stream from an existing list of numbers:
-  val ns: List[Int] = List.fill(100)(1)
-  val nStream: ???  = ???
+  val ns: List[Int]                       = List.fill(100)(1)
+  val nStream: ZStream[Any, Nothing, Int] = ZStream.fromIterable(ns)
 
   // 7. Using repeatEffectOption, repeatedly read lines from the user
   // until the string "EOF" is entered.
-  val allLines: ??? = ???
+  val allLines = ZStream.repeatEffectOption(
+    console.getStrLn.foldM(e => ZIO.fail(Some(e)), {
+      case "EOF"     => ZIO.fail(None)
+      case otherwise => ZIO.succeed(otherwise)
+    })
+  )
 
   // 8. Drain an iterator using `repeatEffectOption`.
-  def drainIterator[A](iterator: Iterator[A]): ZStream[???, ???, A] =
-    ???
+  def drainIterator[A](it: Iterator[A]): ZStream[Any, Throwable, A] =
+    ZStream.repeatEffectOption {
+      ZIO(it.hasNext).mapError(Some(_)).flatMap { hasNext =>
+        if (hasNext) ZIO(it.next).mapError(Some(_))
+        else ZIO.fail(None)
+      }
+    }
+
+  val effectProducingStream = console.getStrLn.flatMap { line =>
+    Task(line.toInt).map(elements => ZStream.fromIterable(List.fill(elements)(5)))
+  }
 
   // 9. Using ZStream.unwrap, unwrap the stream embedded in this effect.
-  val wrapped        = ZIO(ZStream(1, 2, 3))
-  val unwrapped: ??? = ???
+  val wrapped   = ZIO(ZStream(1, 2, 3))
+  val unwrapped = ZStream.unwrap(wrapped)
 
   // 10. Using ZStream.unfold, create a stream that emits the numbers 1 to 10.
-  val oneTo10: ??? = ???
+  val oneTo10: Stream[Nothing, Int] =
+    ZStream.unfold(1) { i =>
+      if (i <= 10) Some((i, i + 1))
+      else None
+    }
 
   // 11. Do the same with unfoldM, but now sleep for `n` milliseconds after
   // emitting every number `n`.
-  val oneTo10WithSleeps: ??? = ???
+  val oneTo10WithSleeps = ZStream.unfoldM(1) { i =>
+    // body
+    ZIO.sleep((i - 1).millis) *>
+      (if (i <= 10) ZIO.succeed(Some((i, i + 1)))
+       else ZIO.succeed(None))
+  }
 
   // 12. Read an array in chunks using unfoldChunkM.
-  def readArray[A](array: Array[A], chunkSize: Int): ZStream[???, ???, A] =
-    ???
+  def readArray[A](array: Array[A], chunkSize: Int): ZStream[Any, Nothing, A] =
+    ZStream.unfoldChunkM(0) { currIdx =>
+      UIO {
+        if (currIdx >= array.size) None
+        else {
+          val chunk = Chunk.fromArray(array.slice(currIdx, math.min(currIdx + chunkSize, array.size)))
+          Some((chunk, currIdx + chunkSize))
+        }
+      }
+    }
 
   // 13. Read an array in chunks using paginateM. You'll need to use
   // `flattenChunks` in this exercise.
-  def readArray2[A](array: Array[A], chunkSize: Int): ZStream[???, ???, A] =
-    ???
+  def readArray2[A](array: Array[A], chunkSize: Int): ZStream[Any, Nothing, A] =
+    ZStream
+      .paginateM(0) { currIdx =>
+        UIO {
+          Chunk.fromArray(array.slice(currIdx, math.min(currIdx + chunkSize, array.size))) ->
+            (if ((currIdx + chunkSize) >= array.size) None else Some(currIdx + chunkSize))
+        }
+      }
+      .flattenChunks
 
   // 14. Implement `tail -f`-like functionality using ZStream.
   def tail(path: Path, chunkSize: Int): ZStream[Clock, Throwable, Byte] =
@@ -81,45 +136,72 @@ object TransformingStreams {
   // use them quite a bit as we create stream processing programs.
 
   // 1. Transform a stream of ints to a stream of strings.
-  val warmup: ??? = ZStream(1, 2, 3) ?
+  val warmup: ZStream[Any, Nothing, String] = ZStream(1, 2, 3).map(_.toString)
 
   // 2. Multiply every integer of the stream using a coefficient
   // retrieved effectfully.
   val currentCoefficient: ZIO[random.Random, Nothing, Double] =
     random.nextDoubleBetween(0.5, 0.85)
-  val multiplied: ??? = ZStream.range(1, 10) ?
+  val multiplied: ZStream[console.Console with random.Random, Nothing, Double] =
+    ZStream
+      .range(1, 10)
+      .mapM(i => currentCoefficient.map(_ * i))
+      .mapM(i => console.putStrLn(i.toString).as(i))
 
   // 3. Split every string to separate lines in this stream.
   val lines: ZStream[Any, Nothing, String] =
     ZStream("line1\nline2", "line3\n\nline4\n")
+      .mapConcat(s => s.split('\n'))
 
   // 4. Print out a JSON array from the following stream of strings
   // using intersperse and tap.
   val data = ZStream("ZIO", "ZStream", "ZSink")
+    .map(el => s""""${el}"""")
+    .intersperse("[", ", ", "]")
+    .tap(console.putStrLn(_))
 
   // 5. Read a 100 even numbers from the Random generator.
-  val hundredEvens: ZStream[random.Random, Nothing, Int] = ???
+  val hundredEvens: ZStream[random.Random, Nothing, Int] =
+    ZStream.repeatEffect(random.nextInt).filter(_ % 2 == 0).take(100)
 
   // 6. Read 10 lines from the user, but skip the first 3.
-  val linesDropped: ZStream[console.Console, IOException, String] = ???
+  val linesDropped: ZStream[console.Console, IOException, String] =
+    ZStream.repeatEffect(console.getStrLn).take(10).drop(3)
+
+  // ZStream.repeatEffectWith(console.getStrLn, Schedule.recurs(10))
+  // ZStream.repeatEffect(console.getStrLn).take(10)
 
   // 7. Read 10 lines from the user, but drop all lines until the user
   // writes the word "START". Don't include "START" in the stream.
-  val finalEx: ZStream[console.Console, IOException, String] = ???
+  val finalEx: ZStream[console.Console, IOException, String] =
+    ZStream.repeatEffect(console.getStrLn).dropUntil(_ == "START").take(10)
 
-  // 7. Using ZStream#++, print a message to the user, then read a line.
-  val printAndRead: ZStream[console.Console, IOException, String] = ???
+  // 8. Using ZStream#++, print a message to the user, then read a line.
+  val printAndRead: ZStream[console.Console, IOException, String] =
+    ZStream.fromEffect(console.putStrLn("enter your name")).drain ++
+      ZStream.fromEffect(console.getStrLn)
 
-  // 8. Split the following stream of CSV data to individual tokens
-  // that are emitted on a 2-second schedule
+  // 9. Split the following stream of CSV data to individual tokens
+  // that are emitted on a 2-second schedule, using ZStream#flatMap
+  // and ZStream#schedule.
   val scheduled: ZStream[???, Nothing, String] =
-    ZStream("DDOG,12.7,12.8", "NET,10.1,10.2") ?
+    ZStream("DDOG,12.7,12.8", "NET,10.1,10.2")
+      .flatMap { line =>
+        ZStream
+          .fromIterable(line.split(","))
+          .schedule(Schedule.fixed(2.seconds))
+      }
+      .tap(console.putStrLn(_))
 
-  // 9. Terminate this infinite stream as soon as a `Left` is emitted.
-  val terminateOnLeft: ZStream[random.Random, Nothing, ???] =
-    ZStream.repeatEffect(random.nextBoolean.map(if (_) Left(()) else Right(()))) ?
+  // 11. Terminate this infinite stream as soon as a `Left` is emitted.
+  val terminateOnLeft: ZStream[random.Random, Nothing, Unit] =
+    ZStream.repeatEffect(random.nextBoolean.map(if (_) Left(()) else Right(())))
+      .collectWhile {
+        case Right(value) => value
+      }
 
-  // 10. Do the same but with `Option` and `None`:
-  val terminateOnNone: ZStream[random.Random, Nothing, ???] =
-    ZStream.repeatEffect(random.nextBoolean.map(if (_) Some(()) else None)) ?
+  // 12. Do the same but with `Option` and `None`:
+  val terminateOnNone: ZStream[random.Random, Nothing, Unit] =
+    ZStream.repeatEffect(random.nextBoolean.map(if (_) Some(()) else None))
+      .collectWhileSome
 }
--- src/main/scala/streams/workshop/03-resources.scala
+++ src/main/scala/streams/workshop/03-resources.scala
@@ -7,12 +7,28 @@ import java.nio.file.FileSystems
 import java.nio.file.StandardWatchEventKinds
 import scala.jdk.CollectionConverters._
 import java.nio.file.WatchEvent
+import java.io.FileInputStream
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
 
 object Resources {
   // Resource management is an important part of stream processing. Resources can be
   // opened and closed throughout the stream's lifecycle, and most importantly,
   // need to be kept open for precisely as long as they are required for processing
   // the stream's data.
+
+  // val stream = ZStream.bracket(console.putStrLn("Acquiring").as(5))(i => console.putStrLn(s"Releasing ${i}"))
+  //   .flatMap { acquiredResource =>
+  //     // resource lifetime
+  //     ZStream.repeatEffect(console.putStrLn(s"Using resource ${acquiredResource}"))
+  //       .mapConcat()
+  //   }
+
+  // val stream = ZStream.managed(ZManaged.make(console.putStrLn("Acquiring").as(5))(i => console.putStrLn(s"Releasing ${i}")))
+  //   .flatMap { acquiredResource =>
+
+  //   }
 
   class DatabaseClient(clientId: String) {
     def readRow: URIO[random.Random, String] = random.nextString(5).map(str => s"${clientId}-${str}")
@@ -26,31 +42,137 @@ object Resources {
 
   // 1. Create a stream that allocates the database client, reads 5 rows, writes
   // them back to the client and ends.
-  val fiveRows: ??? = ZStream.empty ?
+  val fiveRows = ZStream
+    .bracket(DatabaseClient.make("id-here"))(_.close)
+    .flatMap(client =>
+      ZStream
+        .repeatEffect(client.readRow)
+        .take(5)
+        .tap(client.writeRow)
+    )
 
   // 2. Create a stream that reads 5 rows from 3 different database clients, and writes
   // them to a fourth (separate!) client, closing each reading client after finishing reading.
-  val fifteenRows: ??? = ZStream.empty ?
+  def readDatabase(clientId: String, nRows: Int) =
+    ZStream.bracket(DatabaseClient.make(clientId))(_.close).flatMap { client =>
+      ZStream.repeatEffect(client.readRow).take(nRows.toLong)
+    }
+  val fifteenRows =
+    ZStream.bracket(DatabaseClient.make("writing-client"))(_.close).flatMap { writingClient =>
+      ZStream
+        .concatAll(
+          Chunk(
+            readDatabase("one", 5),
+            readDatabase("two", 5),
+            readDatabase("three", 5)
+          )
+        )
+        .tap(writingClient.writeRow)
+    }
 
   // 3. Read 25 rows from 3 different database clients, and write the rows to 5 additional
   // database clients - 5 rows each. Hint: use ZManaged.scope.
-  val scopes: ??? = ZStream.empty ?
+  def makeManagedClient(clientId: String) = DatabaseClient.make(clientId).toManaged(_.close)
+
+  //      10            10      5       <- reading clients
+  // | --------- | --------- | --- |
+  //
+  // |---|---|---|---|...               <- writing clients
+  //
+
+  val scopes = ZStream.fromEffect(Ref.make(0)).flatMap { rowsRead =>
+    ZStream.managed(ZManaged.scope).flatMap { openWriter =>
+      ZStream.fromEffect(openWriter(makeManagedClient("initial"))).flatMap {
+        case (initFinalizer, initClient) =>
+          ZStream.fromEffect(Ref.make((initFinalizer, initClient))).flatMap { clientRef =>
+            ZStream
+              .concatAll(
+                Chunk(
+                  readDatabase("one", 10),
+                  readDatabase("two", 10),
+                  readDatabase("three", 5)
+                )
+              )
+              .tap { row =>
+                val writeRow = clientRef.get.flatMap(_._2.writeRow(row))
+                val switchOut = clientRef.get.flatMap(_._1(Exit.unit)) *>
+                  openWriter(makeManagedClient("next")).flatMap {
+                    case (nextFinalizer, nextClient) =>
+                      clientRef.set(nextFinalizer -> nextClient) *> nextClient.writeRow(row)
+                  }
+
+                rowsRead.modify { currRows =>
+                  if (currRows == 5) (switchOut, 0)
+                  else (writeRow, currRows + 1)
+                }.flatten
+              }
+          }
+      }
+    }
+  }
+
+  val forComp = for {
+    rowsRead         <- ZStream.fromEffect(Ref.make(0))
+    openWriter       <- ZStream.managed(ZManaged.scope)
+    initFinAndClient <- ZStream.fromEffect(openWriter(makeManagedClient("initial")))
+    clientRef        <- ZStream.fromEffect(Ref.make((initFinAndClient._1, initFinAndClient._2)))
+    row <- ZStream
+            .concatAll(
+              Chunk(
+                readDatabase("one", 10),
+                readDatabase("two", 10),
+                readDatabase("three", 5)
+              )
+            )
+            .tap { row =>
+              val writeRow = clientRef.get.flatMap(_._2.writeRow(row))
+              val switchOut = clientRef.get.flatMap(_._1(Exit.unit)) *>
+                openWriter(makeManagedClient("next")).flatMap {
+                  case (nextFinalizer, nextClient) =>
+                    clientRef.set(nextFinalizer -> nextClient) *> nextClient.writeRow(row)
+                }
+
+              rowsRead.modify { currRows =>
+                if (currRows == 5) (switchOut, 0)
+                else (writeRow, currRows + 1)
+              }.flatten
+            }
+  } yield row
 }
 
 object FileIO {
   // 1. Implement the following combinator for reading bytes from a file using
   // java.io.FileInputStream.
-  def readFileBytes(path: String): ZStream[???, ???, Byte] = ???
+  def readFileBytes(path: String): ZStream[blocking.Blocking, IOException, Byte] =
+    ZStream.fromInputStreamEffect(ZIO(new FileInputStream(path)).refineToOrDie[IOException])
 
   // 2. Implement the following combinator for reading characters from a file using
   // java.io.FileReader.
-  def readFileChars(path: String): ZStream[???, ???, Char] = ???
+  def readFileChars(path: String): ZStream[blocking.Blocking, Throwable, Char] =
+    ZStream.bracket(ZIO(new java.io.FileReader(path)))(reader => UIO(reader.close())).flatMap { reader =>
+      ZStream.repeatEffectChunkOption {
+        for {
+          buf       <- UIO(Array.ofDim[Char](2048))
+          charsRead <- blocking.effectBlocking(reader.read(buf)).mapError(Some(_))
+          result <- if (charsRead == -1) ZIO.fail(None)
+                   else UIO(Chunk.fromArray(buf.slice(0, charsRead)))
+        } yield result
+      }
+    }
 
   // 3. Recursively enumerate all files in a directory.
-  def listFilesRecursive(path: String): ZStream[???, ???, Path] = ???
+  def listFilesRecursive(path: String): ZStream[console.Console, Throwable, Path] =
+    ZStream
+      .bracket(ZIO(Files.walk(Paths.get(path))))(st => UIO(st.close()))
+      .flatMap(ZStream.fromJavaStream(_))
+      .tap(path => console.putStrLn(path.toString()))
 
   // 4. Read data from all files in a directory tree.
-  def readAllFiles(path: String): ZStream[???, ???, Char] = ???
+  def readAllFiles(path: String) =
+    listFilesRecursive(path)
+      .filter(Files.isRegularFile(_))
+      .take(2)
+      .flatMap(path => readFileChars(path.toString))
 
   // 5. Monitor a directory for new files using Java's WatchService.
   // Imperative example:
@@ -65,7 +187,7 @@ object FileIO {
       for (watchEvent <- key.pollEvents().asScala) {
         watchEvent.kind match {
           case StandardWatchEventKinds.ENTRY_CREATE =>
-            val pathEv = watchEvent.asInstanceOf[WatchEvent[Path]]
+            val pathEv   = watchEvent.asInstanceOf[WatchEvent[Path]]
             val filename = pathEv.context()
 
             println(s"${filename} created")
@@ -76,8 +198,29 @@ object FileIO {
     }
   }
 
+  def monitorFileCreation(path: String) =
+    ZStream.managed(ZManaged.fromAutoCloseable(ZIO(FileSystems.getDefault().newWatchService()))).flatMap { watcher =>
+      ZStream.fromEffect(ZIO(Paths.get(path).register(watcher, StandardWatchEventKinds.ENTRY_CREATE))).drain ++
+        ZStream.repeatEffectChunkOption {
+          for {
+            key <- ZIO(watcher.take()).mapError(Some(_))
+            events <- ZIO(
+                       key
+                         .pollEvents()
+                         .asScala
+                         .filter(_.kind() == StandardWatchEventKinds.ENTRY_CREATE)
+                         .map { watchEvent =>
+                           val pathEv   = watchEvent.asInstanceOf[WatchEvent[Path]]
+                           val filename = pathEv.context()
 
-  def monitorFileCreation(path: String): ZStream[???, ???, Path] = ???
+                           filename
+                         }
+                     ).mapError(Some(_))
+            continue <- ZIO(key.reset()).mapError(Some(_))
+            _        <- ZIO.fail(None).when(!continue)
+          } yield Chunk.fromIterable(events)
+        }
+    }
 
   // 6. Write a stream that synchronizes directories.
   def synchronize(source: String, dest: String): ??? = ???
